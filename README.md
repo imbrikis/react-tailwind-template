55b2faecb9e6a2d73068155022790e20bcd302d3
imbrikis <imbrikis@gmail.com>
2022-01-21 11:26:46 -0700

    update readme
    - add styling/basic example to an h1 tag

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # A React and Tailwind template
+
+### How to setup dev environment:
+
+- Clone this repo
+- Run `npm install`
+- You're all set!
+
+### Refer to the Tailwind docs for further information [here](https://tailwindcss.com/docs)
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>React and Tailwind App Template</h1>
+      <h1 className='bg-blue-100 text-center text-xl'>
+        React and Tailwind App Template
+      </h1>
     </div>
   )
 }
